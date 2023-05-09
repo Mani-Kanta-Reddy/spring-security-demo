e6902f7344e9235556eb349a08c80532b889dab9
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-05-09 16:53:26 +0530

    authentication with inMemoryUsers

---
+++ src/main/java/com/neon/springsecuritydemo/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.neon.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+            .withUser("alice")
+            .password("secret")
+            .roles("USER")
+            .and()
+            .withUser("bob")
+            .password("password")
+            .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
