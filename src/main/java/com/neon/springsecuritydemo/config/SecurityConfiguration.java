c1528129a62b5beda5bd9c486598cde5e92cb513
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-05-10 13:05:23 +0530

    authentication/authorization with embedded h2 DB & default schema

--- src/main/java/com/neon/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/com/neon/springsecuritydemo/config/SecurityConfiguration.java
@@ -1,28 +1,62 @@
 package com.neon.springsecuritydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
+    //In memory authentication
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception
+//    {
+//        auth.inMemoryAuthentication()
+//            .withUser("alice")
+//            .password("secret")
+//            .roles("USER")
+//            .and()
+//            .withUser("bob")
+//            .password("password")
+//            .roles("ADMIN");
+//    }
+
+
+    //DataSource
+    private final DataSource dataSource;
+
+    public SecurityConfiguration(@Autowired DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
+
+    //Embedded DB (h2) authentication with default schema
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception
     {
-        auth.inMemoryAuthentication()
-            .withUser("alice")
-            .password("secret")
-            .roles("USER")
-            .and()
-            .withUser("bob")
-            .password("password")
-            .roles("ADMIN");
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(
+                User.withUsername("user")
+                    .password("pass")
+                    .roles("USER")
+            )
+            .withUser(
+                User.withUsername("admin")
+                    .password("pass")
+                    .roles("ADMIN")
+            );
     }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder()
